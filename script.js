492f8f305125dd59c6f0365ee2afc08c7436fb58
Tyler Wyles <tylerwyles@gmail.com>
2023-02-21 09:43:03 -0500

    changed Modal code and styling

--- script.js
+++ script.js
@@ -17,10 +17,3 @@ link.onclick = function() {
 span.onclick = function() {
   modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
